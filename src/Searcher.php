efdc785fe56e470e94ee5e6e1471359259bcb009
Cláudio Ribeiro <claudio.santos.ribeiro@gmail.com>
2016-03-30 21:05:34 +0100

    Initial Commit

---
+++ src/Searcher.php
@@ -0,0 +1,80 @@
+<?php
+
+/* 
+   https://packagist.org/packages/guzzlehttp/guzzle
+   http://www.sitepoint.com/guzzle-php-http-client/
+   http://docs.guzzlephp.org/en/latest/faq.html	
+*/
+
+require_once '../vendor/autoload.php';
+
+use Guzzle\Http\Client;
+use Guzzle\Http\EntityBody;
+use Guzzle\Http\Message\Request;
+use Guzzle\Http\Message\Response;
+
+class Searcher {
+
+	/**
+ 	 * This method queries the reddit API for searches
+ 	 *
+ 	 * @param $subreddit The subreddit to search
+ 	 * @param $query The term to search for
+ 	 * @param $options The filter used to search
+ 	 * @param $results The number of results to return
+ 	 *
+	**/
+	public function execSearch($subreddit, $query, $options, $results = 10) {
+
+		if( $subreddit == "" ){
+			$subreddit = "php";
+		}
+
+		//Checks if options are valid
+		if ($this->validateOptions($options) !== false && $this->validateLimit($results) !== false) {
+
+			$roundedResults = round( $results );
+		
+			//Executes an http request using guzzle
+		    $client = new Client("https://reddit.com");
+		    $request = $client->get("/r/" . $subreddit . "/search.json?q=" . $query . "&restrict_sr=1&sort=" . $options . "&limit=" . $roundedResults);
+		    $response = $request->send();
+		    
+		    $body = $response->getBody(true);
+			
+			return $body;
+		}
+		else {
+			return false;
+		}
+	}
+
+	/**
+	 * Checks if the option passed is valid, false in case it's not
+	 */
+	protected function validateOptions( $options ){
+
+		$possible = array( 'new', 'hot', 'top', 'relevance', 'comments' );
+
+		if( in_array( $options, $possible) ){
+			return $options;
+		} else{
+			return false;
+		}
+
+	}
+	
+	/**
+	 * Checks if the limit passed is valid
+	 */
+	protected function validateLimit( $limit ){
+		
+		if( is_integer( $limit )){
+			return $limit;
+		}else{
+			return false;
+		}
+		
+	}
+
+}
---
+++ usage.php
@@ -0,0 +1,21 @@
+<?php
+
+include_once( 'src/Searcher.php' );
+
+//Calling the storeSearch function search for 'composer' on the 'php' subreddit
+storeSearch( 'php', 'composer', 'hot', 2);
+
+/**
+* This function will execute a search and store the result in a file called data.txt
+*/
+function storeSearch( $subreddit, $query, $options, $results ){
+
+	echo '*** SEARCHING *** ';
+
+	$search = new Searcher();
+	$result = $search->execSearch( $subreddit, $query, $options, $results );
+
+	file_put_contents( 'data.txt', $result, FILE_APPEND);
+
+	echo ' *** SEARCH COMPLETE ***';
+}
