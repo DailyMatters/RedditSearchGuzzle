0d578eb30232c1e0046b134fdb8c6d5180e7f483
DailyMatters <claudio.santos.ribeiro@gmail.com>
2016-10-09 18:30:38 +0100

    Added authenticator class and example

--- oauth.php
+++ oauth.php
@@ -1,45 +1,14 @@
 <?php
-if (isset($_GET["error"]))
-{
-    echo("<pre>OAuth Error: " . $_GET["error"]."\n");
-    echo('<a href="index.php">Retry</a></pre>');
-    die;
-}
+
+include_once( 'src/Authenticator.php' );
 
 $authorizeUrl = 'https://ssl.reddit.com/api/v1/authorize';
 $accessTokenUrl = 'https://ssl.reddit.com/api/v1/access_token';
 $clientId = '';
 $clientSecret = '';
-$userAgent = 'RedditApiClient/0.1 by yourUsernameHere';
+$userAgent = 'RedditApiClient/0.1 by YourUserName';
 
-$redirectUrl =  "http://yourredirecturl.com";
+$redirectUrl =  "YourRedirectURL";
 
-require("/vendor/adoy/oauth2/src/OAuth2/Client.php");
-require("/vendor/adoy/oauth2/src/OAuth2/GrantType/IGrantType.php");
-require("/vendor/adoy/oauth2/src/OAuth2/GrantType/AuthorizationCode.php");
-
-$client = new OAuth2\Client($clientId, $clientSecret, OAuth2\Client::AUTH_TYPE_AUTHORIZATION_BASIC);
-$client->setCurlOption(CURLOPT_USERAGENT,$userAgent);
-
-if (!isset($_GET["code"]))
-{
-    $authUrl = $client->getAuthenticationUrl($authorizeUrl, $redirectUrl, array("scope" => "identity", "state" => "SomeUnguessableValue"));
-    header("Location: ".$authUrl);
-    die("Redirect");
-}
-else
-{
-    $params = array("code" => $_GET["code"], "redirect_uri" => $redirectUrl);
-    $response = $client->getAccessToken($accessTokenUrl, "authorization_code", $params);
-
-    $accessTokenResult = $response["result"];
-    $client->setAccessToken($accessTokenResult["access_token"]);
-    $client->setAccessTokenType(OAuth2\Client::ACCESS_TOKEN_BEARER);
-
-    $response = $client->fetch("https://oauth.reddit.com/api/v1/me.json");
-
-    echo('<strong>Response for fetch me.json:</strong><pre>');
-    print_r($response);
-    echo('</pre>');
-}
-?>
+$auth = new Authenticator( $authorizeUrl, $accessTokenUrl, $clientId, $clientSecret, $userAgent, $redirectUrl );
+$auth->authenticate();
---
+++ src/Authenticator.php
@@ -0,0 +1,68 @@
+<?php
+
+require_once './vendor/autoload.php';
+
+class Authenticator{
+
+	public function __construct( $authorizeUrl, $accessTokenUrl, $clientId, $clientSecret, $userAgent , $redirectUrl ){
+	
+		$this->authorizeUrl = $authorizeUrl;
+		$this->accessTokenUrl = $accessTokenUrl;
+		$this->clientId = $clientId;
+		$this->clientSecret = $clientSecret;
+		$this->userAgent = $userAgent;
+		$this->redirectUrl = $redirectUrl;
+	}
+
+	public function authenticate(){
+	
+		$client = new OAuth2\Client($this->clientId, $this->clientSecret, OAuth2\Client::AUTH_TYPE_AUTHORIZATION_BASIC);
+		$client->setCurlOption(CURLOPT_USERAGENT,$this->userAgent);
+	
+		if (!isset($_GET["code"]))
+		{
+			$authUrl = $client->getAuthenticationUrl($this->authorizeUrl, $this->redirectUrl, array("scope" => "identity", "state" => "SomeUnguessableValue"));
+			header("Location: ".$authUrl);
+			die("Redirect");
+		}
+		else
+		{
+			$this->getUserPreferences( $client, $this->accessTokenUrl );
+		}
+	}
+
+	/*public function getUserPreferences( $client, $accessToken ){
+	
+			$params = array("code" => $_GET["code"], "redirect_uri" => $this->redirectUrl);
+			$response = $client->getAccessToken($accessToken, "authorization_code", $params);
+
+			$accessTokenResult = $response["result"];
+			$client->setAccessToken($accessTokenResult["access_token"]);
+			$client->setAccessTokenType(OAuth2\Client::ACCESS_TOKEN_BEARER);
+
+			$response = $client->fetch("https://oauth.reddit.com/api/v1/me.json");
+
+			echo('<strong>Response for fetch me.json:</strong><pre>');
+			print_r($response);
+			echo('</pre>');
+	
+	}*/
+
+	public function getUserPrivateMessages( $client, $accessToken ){
+		
+			$params = array("code" => $_GET["code"], "redirect_uri" => $this->redirectUrl);
+			$response = $client->getAccessToken($accessToken, "authorization_code", $params);
+
+			$accessTokenResult = $response["result"];
+			$client->setAccessToken($accessTokenResult["access_token"]);
+			$client->setAccessTokenType(OAuth2\Client::ACCESS_TOKEN_BEARER);
+
+			$response = $client->fetch("https://oauth.reddit.com/api/read_all_messages.json");
+
+			echo('<strong>Response for yolo.json:</strong><pre>');
+			print_r($response);
+			echo('</pre>');		
+		
+	}
+
+}
