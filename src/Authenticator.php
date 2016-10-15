82c00b0b0510e4601de7df4b5d4e205691521eb5
Art4 <art4@wlabs.de>
2016-10-15 16:49:07 +0200

    Add tests for Searcher

--- src/Authenticator.php
+++ src/Authenticator.php
@@ -1,11 +1,11 @@
 <?php
 
-require_once './vendor/autoload.php';
+namespace redditSearchGuzzle\Searcher;
 
 class Authenticator{
 
 	public function __construct( $authorizeUrl, $accessTokenUrl, $clientId, $clientSecret, $userAgent , $redirectUrl ){
-	
+
 		$this->authorizeUrl = $authorizeUrl;
 		$this->accessTokenUrl = $accessTokenUrl;
 		$this->clientId = $clientId;
@@ -15,10 +15,10 @@ class Authenticator{
 	}
 
 	public function authenticate(){
-	
+
 		$client = new OAuth2\Client($this->clientId, $this->clientSecret, OAuth2\Client::AUTH_TYPE_AUTHORIZATION_BASIC);
 		$client->setCurlOption(CURLOPT_USERAGENT,$this->userAgent);
-	
+
 		if (!isset($_GET["code"]))
 		{
 			$authUrl = $client->getAuthenticationUrl($this->authorizeUrl, $this->redirectUrl, array("scope" => "identity", "state" => "SomeUnguessableValue"));
@@ -32,7 +32,7 @@ class Authenticator{
 	}
 
 	public function getUserPreferences( $client, $accessToken ){
-	
+
 			$params = array("code" => $_GET["code"], "redirect_uri" => $this->redirectUrl);
 			$response = $client->getAccessToken($accessToken, "authorization_code", $params);
 
@@ -45,11 +45,11 @@ class Authenticator{
 			echo('<strong>Response for fetch me.json:</strong><pre>');
 			print_r($response);
 			echo('</pre>');
-	
+
 	}
 
 	public function getUserPrivateMessages( $client, $accessToken ){
-		
+
 			$params = array("code" => $_GET["code"], "redirect_uri" => $this->redirectUrl);
 			$response = $client->getAccessToken($accessToken, "authorization_code", $params);
 
@@ -61,8 +61,8 @@ class Authenticator{
 
 			echo('<strong>Response for yolo.json:</strong><pre>');
 			print_r($response);
-			echo('</pre>');		
-		
+			echo('</pre>');
+
 	}
 
 }
