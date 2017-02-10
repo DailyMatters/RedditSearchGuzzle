b38af7c5b15d41c245214f5063690db30a31ef39
Cláudio Ribeiro <claudio.santos.ribeiro@gmail.com>
2017-02-10 11:55:29 +0000

    Update oauth.php

--- oauth.php
+++ oauth.php
@@ -10,7 +10,7 @@ $clientId = '...';
 $clientSecret = '...';
 $userAgent = '...';
 
-$redirectUrl =  "http://127.0.0.1/NewGuzzle/RedditSearchGuzzle/oauth.php";
+$redirectUrl =  "...";
 
 $auth = new Authenticator( $authorizeUrl, $accessTokenUrl, $clientId, $clientSecret, $userAgent, $redirectUrl );
 $auth->authenticate();
